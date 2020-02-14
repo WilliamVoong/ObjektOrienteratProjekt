e35ff5d6a84059eff65aef55ba83c55dfaf212ed
William <lewivo@gmail.com>
2020-02-14 09:23:39 +0100

    added inputchecker and exception

---
+++ objektorienterat/src/objektorienterat/StringEmptyException.java
@@ -0,0 +1,9 @@
+package src.objektorienterat;
+
+public class StringEmptyException extends Exception {
+	
+	StringEmptyException(String string){
+		super("the String is empty Insert a nonempty string", new Throwable());
+	}
+
+}
