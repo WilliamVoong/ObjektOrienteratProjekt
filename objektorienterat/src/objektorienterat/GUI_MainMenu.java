2f677aef6249dc4ca8b1a7c1cc21ca4311f4a137
William <lewivo@gmail.com>
2020-02-23 16:45:52 +0100

    D:

--- objektorienterat/src/objektorienterat/GUI_MainMenu.java
+++ objektorienterat/src/objektorienterat/GUI_MainMenu.java
@@ -22,6 +22,7 @@ import java.awt.*;
 
 public class GUI_MainMenu extends DisplayScreen {
 
+<<<<<<< Updated upstream
 
 
 	private FileHandler filehandler;
@@ -30,6 +31,15 @@ public class GUI_MainMenu extends DisplayScreen {
 
 
 	GUI_MainMenu(SwappableScreen layoutManager ,  FileHandler filehandler, GameAdmin gameAdmin) {
+=======
+	
+	GameController controller;  
+	FileHandler filehandler; 
+	Player player; 
+	
+	
+	GUI_MainMenu(SwappableScreen layoutManager ,Player player controller) {
+>>>>>>> Stashed changes
 
 		super(layoutManager);
 		this.filehandler=filehandler;
